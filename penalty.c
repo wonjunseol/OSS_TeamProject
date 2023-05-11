ea3cb5eb1829bb16efee0cc920e62fd75d06152b
설원준 <seol-wonjun@seol-wonjun-ui-MacBookAir.local>
2023-05-11 10:56:40 +0900

    updated penalty.c

--- penalty.c
+++ penalty.c
@@ -1,1 +1,17 @@
-
+int selectMenu(){
+    int menu;
+    printf("\n*** HGU Penalty System ***\n");
+    printf("1. 벌점 추가\n"); 
+    printf("2. 벌점 삭제\n"); 
+    printf("3. 벌점 수정\n"); 
+    printf("4. 벌점 조회\n"); 
+    printf("5. 벌점 검색\n"); 
+    printf("6. 벌점 사례 검색\n");
+    printf("7. 파일 저장\n");
+    printf("8. 전체삭제\n");
+    printf("공지사항 및 벌점 종류");
+    printf("0. 종료\n\n");
+    printf("=> 원하는 메뉴는? ");
+    scanf("%d", &menu);
+    return menu;
+}
