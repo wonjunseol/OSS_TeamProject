974392efc00910e3d5f74ffb02e566bb0c576c04
Hakyoungan <hakyoung8461@gmail.com>
2023-05-12 22:06:43 +0900

    add structure

--- penalty.h
+++ penalty.h
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+
+typedef struct {
+	char name[30];
+	int studentId;
+	unsigned char totalNum;
+	char teamProf[30];
+	char rcName[30];
+	int roomNum;
+	char apply; //Y/N
+}student;
