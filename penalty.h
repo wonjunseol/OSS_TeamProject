46f95e241379cabd617488ac91851487afca1624
Hakyoungan <128044721+Hakyoungan@users.noreply.github.com>
2023-05-20 23:57:10 +0900

    Update penalty.h

--- penalty.h
+++ penalty.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct {
 	char name[30];
@@ -10,3 +11,21 @@ typedef struct {
 	int roomNum;
 	char apply; //Y/N
 }student;
+
+int addStudent(student *plist, int count);
+void goStudent(student *plist, int count);
+void goNum(student *plist, int count);
+void readStudent(student plist);
+void listStudent(student *plist, int count);
+int updateStudent(student *plist);
+int selectDataNo(student *plist, int count);
+void goCouncel(student *plist, int count);
+int goDelete(student *plist, int count);
+int selectno(student *s,int count);
+int deletePenalty(student *s);
+int deleteStudent(student *s);
+void searchPenalty(student *s,int count);
+void saveData(student *s, int count);
+int loadData(student s[], int count);
+void showInfo();
+int searchID(student *s, int count);
