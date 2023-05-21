2d6ba0d50f97e51bd3e097bfee2cca0f86a70be3
설원준 <seol-wonjun@seol-wonjun-ui-MacBookAir.local>
2023-05-21 21:52:59 +0900

    update penalty.c

--- penalty.c
+++ penalty.c
@@ -58,7 +58,7 @@ void searchPenalty(student *s,int count){
     printf("찾으시는 총벌점은?");
     scanf("%d",&search);
     for(int i=0;i<count;i++){
-        if(s[i].totalNum==-1){continue;}
+        if(s[i].totalNum==0){continue;}
         if(s[i].totalNum==search){
 		printf("*****************\n");
          	readStudent(s[i]);
@@ -81,7 +81,7 @@ void saveData(student *s, int count){
     printf("=>저장됨!");
 }
 
-int loadData(student *s){
+int loadData(student s[], int count){
     int i=0;
     FILE *fp;
     fp=fopen("penalty.txt","rt");
@@ -89,22 +89,23 @@ int loadData(student *s){
         printf("파일 없음\n");
         return 0;
     }
-    for(;i<100;i++){
+    for(;i<count;i++){
         fscanf(fp,"%s",s[i].name);
         if(feof(fp)) break;
         fscanf(fp,"%d",&s[i].studentId);
-        fscanf(fp,"%u",&s[i].totalNum);
-        fscanf(fp,"%s",&s[i].teamProf); 
+        fscanf(fp,"%s",s[i].teamProf); 
         fscanf(fp,"%s",s[i].rcName);
-        fscanf(fp,"%d",s[i].roomNum);
-        fscanf(fp,"%c",s[i].apply);        
-}    fclose(fp);
+        fscanf(fp,"%d",&s[i].roomNum);
+        fscanf(fp,"%d",&s[i].totalNum);
+        fscanf(fp," %c",&s[i].apply);        
+    }
+    fclose(fp);
     printf("=>로딩 성공!\n");
     return i;
 }
 
 void readStudent(student plist) {
-    printf("	 %s %d %s교수님 %s %d\n", plist.name, plist.studentId, plist.teamProf, plist.rcName, plist.roomNum);
+    printf("    %s %d %s교수님 %s %d 총벌점: %d점 상담신청 여부: %c\n", plist.name, plist.studentId, plist.teamProf, plist.rcName, plist.roomNum, plist.totalNum, plist.apply);
 }
 
 void listStudent(student *plist, int count) {
@@ -117,25 +118,10 @@ void listStudent(student *plist, int count) {
         readStudent(plist[i]);
     }
 }
-<<<<<<< HEAD
 
-=======
 void showInfo(){
 printf("--한동대학교 생활관 공지사항 및 벌점 종류--\n\n");
-printf("한동대학교의 학생 생활관은 "공동 생활 공간"으로 나를 지키고 타인을 배려하는 마음으로 함께 수칙을 지켜나갑시다.\n\n
-*생활관 건물 내에서 음주 및 흡연을 하는 학생이 적발시 운영내규 벌점기준표에 의거하여 퇴사 조치대상이므로 기숙사내에서는 절대로 음주와 흡연을 금지하여 주시길 바랍니다.*\n\n
-무단 외박 - 1점\n
-생활관 내에 외부인 출입 및 숙박 - 2점\n
-공동사용구역에 음식물 및 쓰레기무단투기 - 2점\n
-인원점검표에 허위기재 - 3점\n
-생활관 관련 시설물을 고의로 파괴 또는 훼손 - 3점\n
-형사상 위범 행위자(절도. 폭력) - 4점\n
-생활관 내에서 음주, 흡연 - 퇴거\n
-남.여 기숙사 무단 출입 - 퇴거\n\n
-
-*벌점 7점시 팀교수님 면담*\n
-*벌점 10점시 퇴거*\n")
->>>>>>> 3d12585df3109978d6a1013b6a448978a13606ef
+printf("한동대학교의 학생 생활관은 \"공동 생활 공간\"으로 나를 지키고 타인을 배려하는 마음으로 함께 수칙을 지켜나갑시다.\n\n*생활관 건물 내에서 음주 및 흡연을 하는 학생이 적발시 운영내규 벌점기준표에 의거하여 퇴사 조치대상이므로 기숙사내에서는 절대로 음주와 흡연을 금지하여 주시길 바랍니다.*\n\n무단 외박 - 1점\n생활관 내에 외부인 출입 및 숙박 - 2점\n공동사용구역에 음식물 및 쓰레기무단투기 - 2점\n인원점검표에 허위기재 - 3점\n생활관 관련 시설물을 고의로 파괴 또는 훼손 - 3점\n형사상 위범 행위자(절도. 폭력) - 4점\n생활관 내에서 음주, 흡연 - 퇴거\n남.여 기숙사 무단 출입 - 퇴거\n\n*벌점 7점시 팀교수님 면담*\n*벌점 10점시 퇴거*\n")
 }
 
 int addStudent(student *plist, int count) {
