866cce9cbed8ca155ac193d2a9dc94732a2f5b92
wonjunseol <130718201+wonjunseol@users.noreply.github.com>
2023-05-21 22:08:51 +0900

    Update entire penalty.c

--- penalty.c
+++ penalty.c
@@ -38,70 +38,20 @@ int searchID(student *s, int count){
     return -1;
 }
 
-
-int deletePenalty(student *s){
-    printf("몇점을 삭제하시겠습니까?");
-    int num;
-    scanf("%d",&num);
-    s->totalNum-=num;
+int addStudent(student *plist, int count) {
+    int check = 0;
+    getchar();
+    printf("\n");
+    listStudent(plist, count);
+    printf("학생을 추가하시겠습니까? 아니면 기존에 있는 학번에 벌점을 추가하시겠습니까?(학생 추가: 0, 벌점 추가: 1) ");
+    scanf("%d", &check);
+    if(check == 0) {
+        goStudent(plist, count);
+    }
+    else if(check ==1) {
+        goNum(plist,count);
+    }
     return 1;
-}
-
-int deleteStudent(student *s){
-    s->totalNum=0;
-    return 1;
-}
-
-void searchPenalty(student *s,int count){
-    int snt=0;
-    int search;
-    printf("찾으시는 총벌점은?");
-    scanf("%d",&search);
-    for(int i=0;i<count;i++){
-        if(s[i].totalNum==0){continue;}
-        if(s[i].totalNum==search){
-		printf("*****************\n");
-         	readStudent(s[i]);
-		snt++;
-        }
-    }
-    if(snt==0){
-        printf("검색된 데이터 없음\n");
-    }
-}
-
-void saveData(student *s, int count){
-    FILE *fp;
-    fp=fopen("penalty.txt","wt");
-    for(int i=0;i<count;i++){
-        if(s[i].totalNum==-1) continue;
-        fprintf(fp,"%s %d %s %s %d %d %c\n",s[i].name,s[i].studentId,s[i].teamProf,s[i].rcName,s[i].roomNum,s[i].totalNum,s[i].apply);
-    }
-    fclose(fp);
-    printf("=>저장됨!");
-}
-
-int loadData(student s[], int count){
-    int i=0;
-    FILE *fp;
-    fp=fopen("penalty.txt","rt");
-    if(fp==NULL){
-        printf("파일 없음\n");
-        return 0;
-    }
-    for(;i<count;i++){
-        fscanf(fp,"%s",s[i].name);
-        if(feof(fp)) break;
-        fscanf(fp,"%d",&s[i].studentId);
-        fscanf(fp,"%s",s[i].teamProf); 
-        fscanf(fp,"%s",s[i].rcName);
-        fscanf(fp,"%d",&s[i].roomNum);
-        fscanf(fp,"%d",&s[i].totalNum);
-        fscanf(fp," %c",&s[i].apply);        
-    }
-    fclose(fp);
-    printf("=>로딩 성공!\n");
-    return i;
 }
 
 void readStudent(student plist) {
@@ -114,31 +64,9 @@ void listStudent(student *plist, int count) {
         if(plist[i].totalNum == 0) {
             continue;
         }
-        printf("%2d ", i+1);
         readStudent(plist[i]);
     }
-}
-
-void showInfo(){
-printf("--한동대학교 생활관 공지사항 및 벌점 종류--\n\n");
-printf("한동대학교의 학생 생활관은 \"공동 생활 공간\"으로 나를 지키고 타인을 배려하는 마음으로 함께 수칙을 지켜나갑시다.\n\n*생활관 건물 내에서 음주 및 흡연을 하는 학생이 적발시 운영내규 벌점기준표에 의거하여 퇴사 조치대상이므로 기숙사내에서는 절대로 음주와 흡연을 금지하여 주시길 바랍니다.*\n\n무단 외박 - 1점\n생활관 내에 외부인 출입 및 숙박 - 2점\n공동사용구역에 음식물 및 쓰레기무단투기 - 2점\n인원점검표에 허위기재 - 3점\n생활관 관련 시설물을 고의로 파괴 또는 훼손 - 3점\n형사상 위범 행위자(절도. 폭력) - 4점\n생활관 내에서 음주, 흡연 - 퇴거\n남.여 기숙사 무단 출입 - 퇴거\n\n*벌점 7점시 팀교수님 면담*\n*벌점 10점시 퇴거*\n");
-}
-
-int addStudent(student *plist, int count) {
-    int check = 0;
-    getchar();
     printf("\n");
-    listStudent(plist, count);
-    printf("학생을 추가하시겠습니까? 아니면 기존에 있는 학번에 벌점을 추가하시겠습니까?(학생 추가: 0, 벌점 추가: 1) ");
-    scanf("%d", &check);
-    if(check == 0) {
-        goStudent(plist, count);
-        printf("%s학생이 벌점 1점으로 추가되었습니다.\n", plist[count].name);
-    }
-    else if(check == 1) {
-        goNum(plist, count);
-    }
-    return 1;
 }
 
 void goStudent(student *plist, int count) {
@@ -178,6 +106,30 @@ void goNum(student *plist, int count) {
     if(go == 0) {
         printf("해당 학번의 학생이 없습니다.\n");
     }
+}
+
+int selectDataNo(student *plist, int count) {
+    int no;
+    listStudent(plist, count);
+    printf("\n");
+    printf("수정하고싶은 학생의 번호는 (취소: 0)? ");
+    scanf("%d", &no);
+    return no;
+}
+
+int updateStudent(student *plist) {
+    printf("수정할 학생의 이름은? ");
+    scanf("%s", plist->name);
+    printf("수정할 학생의 학번은? ");
+    scanf("%d", &plist->studentId);
+    printf("수정할 학생의 팀교수님은? ");
+    scanf("%s", plist->teamProf);
+    printf("수정할 학생의 RC는? ");
+    scanf("%s", plist->rcName);
+    printf("수정할 학생의 호실은? ");
+    scanf("%d", &plist->roomNum);
+    printf("=> 수정됨!\n");
+    return 1;
 }
 
 void goCouncel(student *plist, int count) {
@@ -245,17 +197,82 @@ int goDelete(student *plist, int count) {
     return 0;
 }
 
-int updateStudent(student *plist) {
-    printf("수정할 학생의 이름은? ");
-    scanf("%s", plist->name);
-    printf("수정할 학생의 학번은? ");
-    scanf("%d", &plist->studentId);
-    printf("수정할 학생의 팀교수님은? ");
-    scanf("%s", plist->teamProf);
-    printf("수정할 학생의 RC는? ");
-    scanf("%s", plist->rcName);
-    printf("수정할 학생의 호실은? ");
-    scanf("%d", &plist->roomNum);
-    printf("=> 수정됨!\n");
+int selectno(student *s,int count){
+    int no;
+    listStudent(s,count);
+    printf("숫자를 고르시오(취소=0): ");
+    scanf("%d",&no);
+    return no;
+}
+
+int deletePenalty(student *s){
+    printf("몇점을 삭제하시겠습니까? ");
+    int num;
+    scanf("%d",&num);
+    s->totalNum-=num;
     return 1;
-}           
+}
+
+int deleteStudent(student *s){
+    s->totalNum=0;
+    s->studentId=0;
+    return 1;
+}
+
+void searchPenalty(student *s,int count){
+    int snt=0;
+    int search;
+    printf("찾으시는 총벌점은? ");
+    scanf("%d",&search);
+    for(int i=0;i<count;i++){
+        if(s[i].totalNum==0){continue;}
+        if(s[i].totalNum==search){
+         printf("*****************\n");
+         readStudent(s[i]);
+         snt++;
+        }    
+    }
+    if(snt==0){
+        printf("검색된 데이터 없음\n");
+    }
+}
+
+void saveData(student *s, int count){
+    FILE *fp;
+    fp=fopen("penalty.txt","wt");
+    for(int i=0;i<count;i++){
+        if(s[i].totalNum==0) continue;
+        fprintf(fp,"%s %d %s %s %d %d %c\n",s[i].name,s[i].studentId, s[i].teamProf, s[i].rcName, s[i].roomNum, s[i].totalNum,s[i].apply);
+    }
+    fclose(fp);
+    printf("\n");
+    printf("=>저장됨!\n");
+}
+
+int loadData(student s[], int count){
+    int i=0;
+    FILE *fp;
+    fp=fopen("penalty.txt","rt");
+    if(fp==NULL){
+        printf("파일 없음\n");
+        return 0;
+    }
+    for(;i<count;i++){
+        fscanf(fp,"%s",s[i].name);
+        if(feof(fp)) break;
+        fscanf(fp,"%d",&s[i].studentId);
+        fscanf(fp,"%s",s[i].teamProf); 
+        fscanf(fp,"%s",s[i].rcName);
+        fscanf(fp,"%d",&s[i].roomNum);
+        fscanf(fp,"%d",&s[i].totalNum);
+        fscanf(fp," %c",&s[i].apply);        
+    }
+    fclose(fp);
+    printf("=>로딩 성공!\n");
+    return i;
+}
+
+void showInfo(){
+    printf("--한동대학교 생활관 공지사항 및 벌점 종류--\n\n");
+    printf("한동대학교의 학생 생활관은 \"공동 생활 공간\"으로 나를 지키고 타인을 배려하는 마음으로 함께 수칙을 지켜나갑시다.\n\n*생활관 건물 내에서 음주 및 흡연을 하는 학생이 적발시 운영내규 벌점기준표에 의거하여 퇴사 조치대상이므로 기숙사내에서는 절대로 음주와 흡연을 금지하여 주시길 바랍니다.*\n\n무단 외박 - 1점\n생활관 내에 외부인 출입 및 숙박 - 2점\n공동사용구역에 음식물 및 쓰레기무단투기 - 2점\n인원점검표에 허위기재 - 3점\n생활관 관련 시설물을 고의로 파괴 또는 훼손 - 3점\n형사상 위범 행위자(절도. 폭력) - 4점\n생활관 내에서 음주, 흡연 - 퇴거\n남.여 기숙사 무단 출입 - 퇴거\n\n*벌점 7점시 팀교수님 면담*\n*벌점 10점시 퇴거*\n");
+}  
